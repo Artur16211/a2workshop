0d1618297f81d897c3f781cba9f6ae6f10e8b10a
Artur16211 <herrera.alvaradoartu@gmail.com>
2025-01-06 15:33:03 -0600

    test

--- assets/gennav.js
+++ assets/gennav.js
@@ -2,25 +2,25 @@
     const navbar = `
         <nav>
             <div class="nav-container">
-            <div class="megamenu">
-                <a href="#">Proyectos</a>
-                <div class="megamenu-content">
-                <button class="close-btn">&times;</button>
-                <a href="/traducciones/blackopsiiilatino.html">
-                    <img src="../assets/img/bo3logo.png" alt="bo3lalogo"> (PC) Call of Duty: Black Ops III Traducción Español
-                    Latino
-                </a>
-                <a href="/traducciones/blackopslatino.html">
-                    <img src="../assets/img/latinbo.jpg" alt="bolalogo">
-                    (PC) Call of Duty: Black Ops Traducción Español Latino
-                </a>
-                <a href="/traducciones/pq2espanol.html">
-                    <img src="../assets/img/pq2.png" alt="pq2eslogo"> (3DS) Persona Q2 Traducción al Español
-                </a>
+                <a href="/index.html"><img src="../assets/img/logo.png" alt="logo" class="logo_nav"></a>
+                <div class="megamenu">
+                    <a href="#">Proyectos</a>
+                    <div class="megamenu-content">
+                    <button class="close-btn">&times;</button>
+                    <a href="/traducciones/blackopsiiilatino.html">
+                        <img src="../assets/img/bo3logo.png" alt="bo3lalogo"> (PC) Call of Duty: Black Ops III Traducción Español
+                        Latino
+                    </a>
+                    <a href="/traducciones/blackopslatino.html">
+                        <img src="../assets/img/latinbo.jpg" alt="bolalogo">
+                        (PC) Call of Duty: Black Ops Traducción Español Latino
+                    </a>
+                    <a href="/traducciones/pq2espanol.html">
+                        <img src="../assets/img/pq2.png" alt="pq2eslogo"> (3DS) Persona Q2 Traducción al Español
+                    </a>
+                    </div>
                 </div>
-            </div>
-
-            <a href="/contacto.html">Contacto</a>
+                <a href="/contacto.html">Contacto</a>
             </div>
         </nav>
     `;
