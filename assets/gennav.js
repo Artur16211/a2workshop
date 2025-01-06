ce1c4b9f21beed9ea4ac47a6c4b283d220adc304
Artur16211 <herrera.alvaradoartu@gmail.com>
2025-01-06 15:18:04 -0600

    test

--- assets/gennav.js
+++ assets/gennav.js
@@ -1,84 +1,28 @@
 ﻿function generarNavbar() {
     const navbar = `
-        <nav class="navbar navbar-expand-lg navbar-black bg-navbar">
-        <div class="container">
-            <button class="custom-navbar-toggler navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <span class="iconify"
-                    data-icon="streamline:interface-setting-menu-1-button-parallel-horizontal-lines-menu-navigation-three-hamburger"
-                    style="color: white;"></span>
-            </button>
-            <a class="nav-link active text-white hide-desktop titlepage" aria-current="page" href="../index.html">
-                <img class="img" src="../assets/img/Logo.png" alt="A2 Workshop" width="100px" />
-            </a>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active text-white hide-mobile" aria-current="page" href="../index.html"><img
-                                class="img" src="../assets/img/a2logo.png" alt="A2 Workshop" width="200px" /></a>
-                    </li>
-                    <li class="nav-item dropdown hide-mobile2">
-                        <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown5" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
-                            Proyectos
-                        </a>
-                        <ul class="dropdown-menu megamenu text-white bg-black" aria-labelledby="navbarDropdown5">
-                            <div class="row">
-                                <div class="col">
-                                    <ul class="list-unstyled mb-0 mt-4">
-                                        <li>
-                                            <a href="../traducciones/blackopsiiilatino.html"><img class="img-zoom rounded"
-                                                    src="../assets/img/bo3logo.png" alt="imagen1"
-                                                    width="100%" /></a>
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div class="col">
-                                    <ul class="list-unstyled mb-0 mt-4">
-                                        <li>
-                                            <a href="../traducciones/blackopslatino.html"><img class="img-zoom rounded"
-                                                    src="../assets/img/latinbo.jpg" alt="imagen1"
-                                                    width="100%" /></a>
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div class="col">
-                                    <ul class="list-unstyled mb-0 mt-4">
-                                        <li>
-                                            <a href="../traducciones/pq2espanol.html"><img class="img-zoom rounded"
-                                                    src="../assets/img/pq2.png" alt="imagen1" width="100%" /></a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </ul>
-                    </li>
-                    <li class="hide-desktop2 nav-item dropdown">
-                        <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown2" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
-                            Proyectos
-                        </a>
-                        <ul aria-labelledby="navbarDropdown2">
-                            <li>
-                                <a href="../traducciones/blackopsiiilatino.html">Call of Duty: Black Ops III (PC) Español Latino</a>
-                            </li><br>
-                            <li>
-                                <a href="../traducciones/blackopslatino.html">Call of Duty: Black Ops Español Latino</a>
-                            </li>
-                            <br>
-                            <li>
-                                <a href="../traducciones/pq2espanol.html">Persona Q2 Traducción al Español</a>
-                            </li><br>
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="../contacto.html">Contacto</a>
-                    </li>
-                </ul>
+        <nav>
+            <div class="nav-container">
+            <div class="megamenu">
+                <a href="#">Proyectos</a>
+                <div class="megamenu-content">
+                <button class="close-btn">&times;</button>
+                <a href="/traducciones/blackopsiiilatino.html">
+                    <img src="../assets/img/bo3logo.png" alt="bo3lalogo"> (PC) Call of Duty: Black Ops III Traducción Español
+                    Latino
+                </a>
+                <a href="/traducciones/blackopslatino.html">
+                    <img src="../assets/img/latinbo.jpg" alt="bolalogo">
+                    (PC) Call of Duty: Black Ops Traducción Español Latino
+                </a>
+                <a href="/traducciones/pq2espanol.html">
+                    <img src="../assets/img/pq2.png" alt="pq2eslogo"> (3DS) Persona Q2 Traducción al Español
+                </a>
+                </div>
             </div>
-        </div>
-    </nav>
+
+            <a href="/contacto.html">Contacto</a>
+            </div>
+        </nav>
     `;
 
     // Inserta el navbar en el elemento con id "navbar-container"
