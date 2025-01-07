224942833dbeae3a6d4de15317b1e22461c1980e
Artur16211 <herrera.alvaradoartu@gmail.com>
2025-01-06 20:23:24 -0600

    test

--- assets/gennav.js
+++ assets/gennav.js
@@ -26,13 +26,38 @@
         </nav>
     `;
 
-    // Inserta el navbar en el elemento con id "navbar-container"
-    // if in the url is "index.html or contact.html" show another navbar
-    document.getElementById("navbar-container").innerHTML = navbar;
+    const navbar2 = `
+        <nav>
+            <div class="nav-container">
+                <div class="megamenu">
+                    <a href="#">Proyectos</a>
+                    <div class="megamenu-content">
+                    <button class="close-btn">&times;</button>
+                    <a href="/traducciones/blackopsiiilatino.html">
+                        <img src="../assets/img/bo3logo.png" alt="bo3lalogo"> (PC) Call of Duty: Black Ops III Traducción Español
+                        Latino
+                    </a>
+                    <a href="/traducciones/blackopslatino.html">
+                        <img src="../assets/img/latinbo.jpg" alt="bolalogo">
+                        (PC) Call of Duty: Black Ops Traducción Español Latino
+                    </a>
+                    <a href="/traducciones/pq2espanol.html">
+                        <img src="../assets/img/pq2.png" alt="pq2eslogo"> (3DS) Persona Q2 Traducción al Español
+                    </a>
+                    </div>
+                </div>
+                <a href="/contacto.html">Contacto</a>
+            </div>
+        </nav>
+    `;
 
-    // exec message
-    // console.log("Navbar generado");
+    if (window.location.pathname === "/index.html") {
+        document.getElementById("navbar-container").innerHTML = navbar2;
+    }
+    else {
+        document.getElementById("navbar-container").innerHTML = navbar;
+    }
+
 }
 
-// Llama a la función al cargar la página
 generarNavbar();
